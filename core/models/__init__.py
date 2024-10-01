57792b575563c7dbbe360471da623c50c02d3fc4
My Name <fedorsmorodskii@gmail.com>
2024-10-01 16:28:20 +0700

    FastAPI v.2 SQL Table

---
+++ core/models/__init__.py
@@ -0,0 +1,12 @@
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    "Product",
+    # Add more models here..."
+)
+
+
+from .base import Base
+from.db_helper import DatabaseHelper, db_helper
+from .product import Product
